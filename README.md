40b6a4103bd5d6279b748f44ff401135d40abdc3
fengluoye2012 <1522988552@qq.com>
2020-04-27 23:24:39 +0800

    feature：添加headerViewPager module

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CusHeaderViewPager
+
+### git push 流程：
+- https://www.cnblogs.com/yorkmass/p/11109817.html
---
+++ headerviewpager/src/main/java/com/fly/headerviewpager/HeaderViewPager.java
@@ -0,0 +1,20 @@
+package com.fly.headerviewpager;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class HeaderViewPager extends LinearLayout {
+    
+    public HeaderViewPager(Context context) {
+        super(context);
+    }
+
+    public HeaderViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HeaderViewPager(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}

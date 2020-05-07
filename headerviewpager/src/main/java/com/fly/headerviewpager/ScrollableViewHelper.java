27d2da36fb040e143b6d483cfa84bf5858235e36
fengluoye2012 <1522988552@qq.com>
2020-05-07 23:34:15 +0800

    feature：判断scrollableView 是否处于顶部

--- headerviewpager/src/main/java/com/fly/headerviewpager/ScrollableViewHelper.java
+++ headerviewpager/src/main/java/com/fly/headerviewpager/ScrollableViewHelper.java
@@ -1,6 +1,12 @@
 package com.fly.headerviewpager;
 
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ScrollView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 public class ScrollableViewHelper {
@@ -24,6 +30,72 @@ public class ScrollableViewHelper {
 
     public boolean isTop() {
         if (scrollableView == null) {
+            throw new NullPointerException("scrollableView can not null");
+        }
+
+        if (scrollableView instanceof RecyclerView) {
+            return isRecyclerViewTop((RecyclerView) scrollableView);
+        } else if (scrollableView instanceof AdapterView) {
+            return isAdapterViewTop((AdapterView) scrollableView);
+        } else if (scrollableView instanceof ScrollView) {
+            return isScrollViewViewTop((ScrollView) scrollableView);
+        } else if (scrollableView instanceof WebView) {
+            return isWebViewTop((WebView) scrollableView);
+        }
+        return false;
+    }
+
+    /**
+     * 判断RecyclerView 是否处于顶部
+     * <p>
+     * todo 理解getTop、getBottom 等的含义
+     *
+     * @param recyclerView
+     * @return
+     */
+    private boolean isRecyclerViewTop(RecyclerView recyclerView) {
+        RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+        if (layoutManager instanceof LinearLayoutManager) {
+            int firstVisibleItemPosition = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
+            View childAt = recyclerView.getChildAt(0);
+            //recyclerView index = 0 的item 不可见或者 第一条可见的item index 是0并且其top == 0,则RecyclerView 顶部
+            if (childAt == null || (firstVisibleItemPosition == 0 && childAt.getTop() == 0)) {//getTop()相对于parent顶部坐标
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断AdapterView(包括ListView、GridView) 是否处于顶部
+     *
+     * @param adapterView
+     * @return
+     */
+    private boolean isAdapterViewTop(AdapterView adapterView) {
+        int firstVisiblePosition = adapterView.getFirstVisiblePosition();
+        View childAt = adapterView.getChildAt(0);
+        //AdapterView index = 0 的item 不可见或者 第一条可见的item index 是0并且其top == 0,则RecyclerView 顶部
+        if (childAt == null || (firstVisiblePosition == 0 && childAt.getTop() == 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isScrollViewViewTop(ScrollView scrollView) {
+        View childAt = scrollView.getChildAt(0);
+        int scrollY = scrollView.getScrollY();
+
+        if (childAt == null || childAt.getTop() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isWebViewTop(WebView webView) {
+        View childAt = webView.getChildAt(0);
+        int scrollY = webView.getScrollY();
+        if (childAt == null || childAt.getTop() == 0) {
             return true;
         }
         return false;
